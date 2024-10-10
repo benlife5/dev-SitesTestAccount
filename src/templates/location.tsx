182acd94f6658f06e7c81f0748971ebb3a7e07e5
Ben Life <blife@yext.com>
2024-10-10 16:38:06 -0400

    theme 2

--- src/templates/location.tsx
+++ src/templates/location.tsx
@@ -12,6 +12,7 @@ import { DocumentProvider } from "@yext/pages/util";
 import { Config, Render } from "@measured/puck";
 import { locationConfig } from "../ve.config";
 import { applyTheme, resolveVisualEditorData } from "@yext/visual-editor";
+import { themeConfig } from "../../theme.config";
 
 export const config = {
   name: "location",
@@ -73,7 +74,7 @@ export const getHeadConfig: GetHeadConfig<TemplateRenderProps> = ({
         },
       },
     ],
-    other: applyTheme(document),
+    other: applyTheme(document, themeConfig),
   };
 };
 
