ef6dd461aabae23bdde1d3c80068c3a7f3b0ede8
Ben Life <blife@yext.com>
2024-10-31 13:37:16 -0400

    fix

--- src/ve.config.tsx
+++ src/ve.config.tsx
@@ -1,64 +1,65 @@
 import type { Config } from "@measured/puck";
+import "@yext/visual-editor/style.css";
 
-// import {
-//   AddressComponent as Address,
-//   AddressProps,
-//   BodyTextComponent as BodyText,
-//   BodyTextProps,
-//   CTAWrapperComponent as CTA,
-//   CTAWrapperProps,
-//   EmailsComponent as Emails,
-//   EmailsProps,
-//   FlexContainerComponent as FlexContainer,
-//   FlexContainerProps,
-//   GridSectionComponent as GridSection,
-//   GridSectionProps,
-//   HeadingTextComponent as HeadingText,
-//   HeadingTextProps,
-//   HoursCardComponent as HoursCard,
-//   HoursCardProps,
-//   HoursStatusComponent as HoursStatus,
-//   HoursStatusProps,
-//   ImageWrapperComponent as ImageWrapper,
-//   ImageWrapperProps,
-//   PhoneComponent as Phone,
-//   PhoneProps,
-//   TextListComponent as TextList,
-//   TextListProps,
-// } from "@yext/visual-editor";
+import {
+  AddressComponent as Address,
+  AddressProps,
+  BodyTextComponent as BodyText,
+  BodyTextProps,
+  CTAWrapperComponent as CTA,
+  CTAWrapperProps,
+  EmailsComponent as Emails,
+  EmailsProps,
+  FlexContainerComponent as FlexContainer,
+  FlexContainerProps,
+  GridSectionComponent as GridSection,
+  GridSectionProps,
+  HeadingTextComponent as HeadingText,
+  HeadingTextProps,
+  HoursCardComponent as HoursCard,
+  HoursCardProps,
+  HoursStatusComponent as HoursStatus,
+  HoursStatusProps,
+  ImageWrapperComponent as ImageWrapper,
+  ImageWrapperProps,
+  PhoneComponent as Phone,
+  PhoneProps,
+  TextListComponent as TextList,
+  TextListProps,
+} from "@yext/visual-editor";
 import { Header } from "./components/Header.js";
 import { Footer } from "./components/Footer.js";
 
 type LocationProps = {
-  // GridSection: GridSectionProps;
-  // HoursCard: HoursCardProps;
-  // BodyText: BodyTextProps;
-  // HeadingText: HeadingTextProps;
-  // ImageWrapper: ImageWrapperProps;
-  // CTA: CTAWrapperProps;
-  // HoursStatus: HoursStatusProps;
-  // FlexContainer: FlexContainerProps;
-  // Address: AddressProps;
-  // TextList: TextListProps;
-  // Emails: EmailsProps;
-  // Phone: PhoneProps;
+  GridSection: GridSectionProps;
+  HoursCard: HoursCardProps;
+  BodyText: BodyTextProps;
+  HeadingText: HeadingTextProps;
+  ImageWrapper: ImageWrapperProps;
+  CTA: CTAWrapperProps;
+  HoursStatus: HoursStatusProps;
+  FlexContainer: FlexContainerProps;
+  Address: AddressProps;
+  TextList: TextListProps;
+  Emails: EmailsProps;
+  Phone: PhoneProps;
 };
 
 // All the available components for locations
 export const locationConfig: Config<LocationProps> = {
   components: {
-    // Address,
-    // BodyText,
-    // CTA,
-    // FlexContainer,
-    // GridSection,
-    // HeadingText,
-    // HoursCard,
-    // HoursStatus,
-    // ImageWrapper,
-    // TextList,
-    // Emails,
-    // Phone,
+    Address,
+    BodyText,
+    CTA,
+    FlexContainer,
+    GridSection,
+    HeadingText,
+    HoursCard,
+    HoursStatus,
+    ImageWrapper,
+    TextList,
+    Emails,
+    Phone,
   },
   root: {
     render: ({ children, puck: { isEditing } }) => {
