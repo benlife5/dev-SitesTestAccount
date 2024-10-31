7f8e64648c25528b48fc3e732c5a043d9eebae43
Ben Life <blife@yext.com>
2024-10-31 14:09:41 -0400

    add tailwind content

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,10 @@ import { themeConfig } from "./theme.config";
 import { themeResolver } from "@yext/visual-editor";
 
 export default {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+    "./node_modules/@yext/visual-editor/dist/**/*.js",
+  ],
   theme: {
     extend: themeResolver(
       {
